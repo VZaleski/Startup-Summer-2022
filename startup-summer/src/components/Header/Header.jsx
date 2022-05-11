3075e1bedc061887fd08f6ebb0b0d34f371702d8
VZaleski <70795078+VZaleski@users.noreply.github.com>
2022-05-11 22:19:51 +0300

    feat: add component Header

---
+++ startup-summer/src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import s from './Header.module.css';
+import logo from '../../assets/images/git_logo.svg';
+import magniferIcon from '../../assets/images/magnifer_icon.svg';
+
+function Header() {
+  return (
+    <header className={s.header}>
+      <div className={s.wrapper}>
+        <img className={s.logo} src={logo} alt="logo" />
+        <form className={s.search}>
+          <input type="search" placeholder="Search" className={s.search__input} />
+          <button type="submit" className={s.search__button}>
+            <img className={s.search__icon} src={magniferIcon} alt="magnifer" />
+          </button>
+        </form>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
