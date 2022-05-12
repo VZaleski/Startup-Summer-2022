0a1d025fc715757c795d483d6e1c3209d04748da
VZaleski <70795078+VZaleski@users.noreply.github.com>
2022-05-12 22:28:06 +0300

    fix: adaptive design components

--- startup-summer/src/components/Main/Main.jsx
+++ startup-summer/src/components/Main/Main.jsx
@@ -1,15 +1,15 @@
 import s from './Main.module.css';
 import Profile from './Profile/Profile';
-import EmptyList from './EmptyList/EmptyList';
-//  import Repositories from './Repositories/Repositories';
+//  import EmptyList from './EmptyList/EmptyList';
+import Repositories from './Repositories/Repositories';
 
 function Main() {
   return (
     <main className={s.main}>
       <div className={s.wrapper}>
         <Profile />
-        {/*  <Repositories />  */}
-        <EmptyList />
+        <Repositories />
+        {/*  <EmptyList />  */}
       </div>
     </main>
   );
